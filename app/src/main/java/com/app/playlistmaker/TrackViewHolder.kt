b5678473044437c5dfcbf653755992f3cd0c5863
Iv4 <github_isc@list.ru>
2023-05-21 18:05:34 +0300

    Update project; Update Reformat Code;

--- app/src/main/java/com/app/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/app/playlistmaker/TrackViewHolder.kt
@@ -1,25 +1,25 @@
 package com.app.playlistmaker
 
-    import android.view.View
-    import android.widget.ImageView
-    import android.widget.TextView
-    import androidx.recyclerview.widget.RecyclerView
-    import com.bumptech.glide.Glide
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class TrackViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        private val albumsImg: ImageView = itemView.findViewById(R.id.albums_img)
-        private val trackName: TextView = itemView.findViewById(R.id.track_name)
-        private val bandName: TextView = itemView.findViewById(R.id.band_name)
-        private val trackTime: TextView = itemView.findViewById(R.id.track_time)
+    private val albumsImg: ImageView = itemView.findViewById(R.id.albums_img)
+    private val trackName: TextView = itemView.findViewById(R.id.track_name)
+    private val bandName: TextView = itemView.findViewById(R.id.band_name)
+    private val trackTime: TextView = itemView.findViewById(R.id.track_time)
 
-        fun bind(item: Track) {
-            Glide.with(itemView)
-                .load(item.artworkUrl100)
-                .placeholder(R.drawable.placeholderview)
-                .into(albumsImg)
-            trackName.text = item.trackName
-            bandName.text = item.bandName
-            trackTime.text = item.trackTime
-        }
+    fun bind(item: Track) {
+        Glide.with(itemView)
+            .load(item.artworkUrl100)
+            .placeholder(R.drawable.placeholderview)
+            .into(albumsImg)
+        trackName.text = item.trackName
+        bandName.text = item.bandName
+        trackTime.text = item.trackTime
     }
+}
