f93ed55adee0677f20fd5bab08b3227496db79f5
Iv4 <github_isc@list.ru>
2023-05-21 16:35:45 +0300

    Update project; Update TrackViewHolder;

--- app/src/main/java/com/app/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/app/playlistmaker/TrackViewHolder.kt
@@ -14,19 +14,12 @@ class TrackViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
         private val trackTime: TextView = itemView.findViewById(R.id.track_time)
 
         fun bind(item: Track) {
-            val adaptedTrackName = item.trackName
-            val adaptedBandName = item.bandName
-            val adaptedTrackTime = item.trackTime
-
-
             Glide.with(itemView)
                 .load(item.artworkUrl100)
                 .placeholder(R.drawable.placeholderview)
                 .into(albumsImg)
-
-            trackName.text = adaptedTrackName
-            bandName.text = adaptedBandName
-            trackTime.text = adaptedTrackTime
-
+            trackName.text = item.trackName
+            bandName.text = item.bandName
+            trackTime.text = item.trackTime
         }
     }
