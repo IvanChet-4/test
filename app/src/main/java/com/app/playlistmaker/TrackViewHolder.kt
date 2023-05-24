e437a02811d1e5bcceb972cb8014e4e3d0c30f0c
Iv4 <github_isc@list.ru>
2023-05-24 21:51:50 +0300

    Update project;

--- app/src/main/java/com/app/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/app/playlistmaker/TrackViewHolder.kt
@@ -1,5 +1,6 @@
 package com.app.playlistmaker
 
+import android.content.Context
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -7,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import kotlinx.coroutines.withContext
 
 class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
